004dd7d44fe3832679e9a377959b20f1c9de6447
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2025-12-27 15:44:49 -0500

    Fixed a few errors in main.py

--- firmware/main.py
+++ firmware/main.py
@@ -1,8 +1,6 @@
 #import board io stuff
 import board
 import busio
-import os
-
 #import KMK stuff
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
@@ -28,8 +26,8 @@ def switch_unicode_os(macros, mode_entry):
 #set up keyboard object
 keyboard = KMKKeyboard()
 keyboard.diode_orientation = DiodeOrientation.COL2ROW 
-keyboard.col_pins = (board.GP03, board.GP04, board.GP02, board.GP01)
-keyboard.row_pins = (board.GP26, board.GP27, board.GP28, board.GP29)
+keyboard.row_pins = (board.D0, board.D1, board.D2, board.D3)
+keyboard.col_pins = (board.D10, board.D9, board.D8, board.D7)
 #set up macros
 macros = Macros(unicode_mode=UnicodeModeWinC)
 keyboard.modules.append(macros)
@@ -38,22 +36,14 @@ holdtap = HoldTap()
 keyboard.modules.append(holdtap)
 #set up screen
 #Don't forget to download circuitpy library for screen
-i2c_bus = busio.I2C(board.GP_07, board.GP_06)
+i2c_bus = busio.I2C(board.SCL, board.SDA)
 driver = SSD1306(i2c=i2c_bus)
 display = Display(display=driver)
 keyboard.extensions.append(display)
 #configure keyboard
-if os.path.exists("keys.keyconfig"):
+try: #os module is not supported
     #add entries to display
     mode_entry = TextEntry(text="Mode: Keyboard; Unicode: Win", x=0, y=16),
-    entries = [
-            TextEntry(text="Todo Pad", x=0, y=0),
-            mode_entry,
-    ]
-    #add all layers to entries
-    for index, name in enumerate(layer_names):
-        entries.append(TextEntry(text="Layer: " + name, x=0, y=32, layer=index),)
-    display.entries = entries
     #define keymapt stuff
     keymap = []
     layer_names = []
@@ -63,36 +53,42 @@ if os.path.exists("keys.keyconfig"):
     with open("keys.keyconfig", "rb") as file:
         #read a few bytes at a time 
         while True:
-            try:
-                #check for layer or button header
-                header = file.read(1)
-                if header == b"\x02": # read layer
+            #check for layer or button header
+            header = file.read(1)
+            if header == b"\x02": # read layer
+                keymap.append([])
+                layer_names.append(file.read(20).decode("utf-8"))
+                #top row of buttons is same for all
+                keymap.append([KC.HT(switch_unicode_os(macros, mode_entry), KC.LCTRL), KC.NO, KC.NO, KC.TO(len(layer_names))])
+            elif header == b"\x11": #read button
+                #add new button row if required
+                if len(keymap[-1]) == 4:
                     keymap.append([])
-                    layer_names.append(file.read(20).decode("utf-8"))
-                    #top row of buttons is same for all
-                    keymap.append([KC.HT(switch_unicode_os(macros, mode_entry), KC.LCTRL), KC.NO, KC.NO, KC.TO(len(layer_names))])
-                elif header == b"\x11": #read button
-                    #add new button row if required
-                    if len(keymap[-1]) == 4:
-                        keymap.append([])
-                    #get button type, size and value
-                    button_type = file.read(1)
-                    button_size = int.from_bytes(file.read(1), "little")
-                    button_value = file.read(button_size).decode("utf-8")
-                    #do action based on button type
-                    if button_type == k_header:
-                        keymap[-1].append(KC.get(button_value, KC.TRANS))
-                    elif button_type == u_header:
-                        keymap[-1].append(KC.MACRO(button_value))
-                elif header == b'\x03':
-                    break
-            except:
+                #get button type, size and value
+                button_type = file.read(1)
+                button_size = int.from_bytes(file.read(1), "little")
+                button_value = file.read(button_size).decode("utf-8")
+                #do action based on button type
+                if button_type == k_header:
+                    keymap[-1].append(KC.get(button_value, KC.TRANS))
+                elif button_type == u_header:
+                    keymap[-1].append(KC.MACRO(button_value))
+            elif header == b'\x03':
                 break
         #update final layer switch to loop back to the begining
         keymap[-1][0][3] = KC.TO(0)
         keyboard.keymap = keymap
+    #add other entries to display
+    entries = [
+            TextEntry(text="Todo Pad", x=0, y=0),
+            mode_entry,
+    ]
+    #add all layers to entries
+    for index, name in enumerate(layer_names):
+        entries.append(TextEntry(text="Layer: " + name, x=0, y=32, layer=index),)
+    display.entries = entries
 #if no keys.keyconfig, default to empty keyboard
-else:
+except IOError:
     keyboard.keymap = [
         [
             KC.TRANS, KC.TRANS, KC.TRANS, KC.TRANS,
